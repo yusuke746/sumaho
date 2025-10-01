184a6af9d971f598e9f0b7fe5deb275ac311661f
yusuke746 <k.y.0203.pasad@gmail.com>
2025-10-01 10:59:38 +0000

    Remove trailing unused code from index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -68,13 +68,9 @@ const styles = StyleSheet.create({
 });
 
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { CameraComponent } from '@/components/CameraComponent.tsx';
 
-export default function HomeScreen() {
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
