d32d5505a3366f2e6cabc604c5ab6307160ba801
yusuke746 <66020550+yusuke746@users.noreply.github.com>
2025-10-04 13:53:28 +0900

    Revert "fix: remove explore route and add null check for ParallaxScrollView children"

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,6 +27,7 @@ export default function TabLayout() {
         }),
       }}>
       <Tabs.Screen
+        name="explore"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
